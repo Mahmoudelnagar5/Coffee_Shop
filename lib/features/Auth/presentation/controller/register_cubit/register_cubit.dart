e05c8102bcd9d13279895f2525948ee579908a83
mahmoudelnagar5 <elnagarm852@gmail.com>
2024-12-09 20:17:36 +0200

    finish register cubit

---
+++ lib/features/Auth/presentation/controller/register_cubit/register_cubit.dart
@@ -0,0 +1,45 @@
+import 'dart:developer';
+
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+
+import '../../../data/repo/auth_repo.dart';
+
+part 'register_state.dart';
+
+class RegisterCubit extends Cubit<RegisterState> {
+  RegisterCubit(this.authRepo) : super(RegisterInitial());
+  final AuthRepo authRepo;
+
+  String password = "";
+  String email = "";
+
+  Future<void> login(
+      {required String email,
+      required String password,
+      required BuildContext context}) async {
+    try {
+      emit(RegisterLoading());
+
+      var result =
+          await authRepo.signupfirebase(email: email, password: password);
+      result.fold((failure) {
+        emit(
+          RegisterFailure(errorMessage: failure.errorMessage),
+        );
+      }, (success) {
+        emit(RegisterSuccess());
+      });
+    } on Exception catch (e) {
+      log('e: $e');
+    }
+  }
+
+  void setregisteremail({required String n}) {
+    email = n;
+  }
+
+  void setregisterpassword({required String p}) {
+    password = p;
+  }
+}
